f5612a370b82bf177f88e20409d7467a3f4a24b9
wb-lmm246964 <wb-lmm246964@alibaba-inc.com>
2017-04-07 13:54:53 +0800

    feat: 添加简单模拟登录功能

--- Jalon_Blog/routes/auth.js
+++ Jalon_Blog/routes/auth.js
@@ -13,4 +13,30 @@ router.get('/logout', (req, res, next) => {
     res.redirect('/');
 });
 
+/* login in webst */
+router.post('/login', (req, res, next) => {
+    //　模拟数据库查询 start
+    const queryEmail = '453741411@qq.com';
+    const queryPassword = '778450';
+    //　模拟数据库查询 end
+    const { email, password } = req.body;
+    console.log('login param:', req.body);
+    if (email === queryEmail) {
+      if (password === queryPassword) {
+        res.redirect('/');
+      } else {
+        res.send({
+          type: 1,
+          msg: '密码错误!'
+        });
+      }
+    } else {
+        res.status(500);
+        res.send({
+          type: 0,
+          msg: '帐号不存在!'
+        });
+    }
+})
+
 module.exports = router;
