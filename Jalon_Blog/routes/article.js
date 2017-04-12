284144677b026c64dcf542b21f3db12df01d12f2
wb-lmm246964 <wb-lmm246964@alibaba-inc.com>
2017-04-12 18:22:09 +0800

    feat: 添加管理文章页面及路由

--- Jalon_Blog/routes/article.js
+++ Jalon_Blog/routes/article.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const articleModel = require('.././models/articles.js');
 
-// Get Article Create Page
+// Get Article Create Page 文章发布界面
 router.get('/', (req, res, next) => {
     res.render('create', {
       title: 'Article Creating'
+    });
+});
+// Get Article Manage Page 文章管理界面
+router.get('/manage', (req, res, next) => {
+    let result =[];
+    // 用articleModel查询 文章列表
+    articleModel.find({}, (err, data) => {
+      if (err) { console.log(err); }
+      if (!!data) { result = data; }
+      res.render('manage', { 
+        title: 'Jalon\'blog',
+        articles: result
+      });
     });
 });
 
@@ -23,6 +36,24 @@ router.post('/add', (req, res, next) => {
   res.send({
     type: 1,
     msg: '文章发布成功!'
+  });
+});
+
+// 删除文章
+router.delete('/delete', (req, res, next) => {
+  const { title } = req.body;
+  console.log('article_title', title);
+  articleModel.remove({
+    title: title
+  }, (err, data) => {
+    if (err) {console.log(err)}
+    if (!!data) {
+      console.log(data);
+    }
+  });
+  res.send({
+    type: 1,
+    msg: '文章删除成功!'
   });
 });
 
